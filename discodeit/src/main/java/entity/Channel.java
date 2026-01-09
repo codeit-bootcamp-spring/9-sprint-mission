ee99ba926d51fec03a38d7c6c6566edf00cbcfbd
한성재 <anis15740@gmail.com>
2026-01-09 18:55:05 +0900

    2026/01/09 - 2

--- discodeit/src/main/java/entity/Channel.java
+++ discodeit/src/main/java/entity/Channel.java
@@ -1,21 +1,24 @@
-package com.sprint.mission.discodeit.entity;
+package entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Channal {
-    private final String id = UUID.randomUUID().toString();
+    private final UUID id = UUID.randomUUID();
     private long createdAt;
     private long updatedAt;
 
     private String name = "";
 
+    private List<UUID> members = new ArrayList<>();
+
     Channal(String name){
         this.name = name;
-
         this.createdAt = this.updatedAt = System.currentTimeMillis();
     }
 
-    public String getId() {
+    public UUID getId() {
         return id;
     }
 
@@ -38,5 +41,22 @@ public class Channal {
     public void UpdateName(String name){
         this.name = name;
         this.updateUpdateAt();
+    }
+
+    public void AddMember(UUID id){
+        members.add(id);
+        updateUpdateAt();
+    }
+
+    public void RemoveMember(UUID id){
+        boolean ret = members.remove(id);
+
+        if (ret == false){
+            // 실패 시 코드
+        }
+        else {
+            // 성공 시 코드
+            updateUpdateAt();
+        }
     }
 }
