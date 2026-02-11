457da8dcf832700739e2e0eb59142ab5dd1c629a
이용일 <lyi980403>
2026-02-11 18:34:58 +0900

    Postman에서 실í이미지 작업만 í남음

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,8 +1,10 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,14 @@ public class BinaryContentController {
 
     private final BinaryContentService binaryContentService;
 
+    @PostMapping("/create")
+    public ResponseEntity<BinaryContent> create(
+            @RequestBody BinaryContentCreateRequest request
+    ) {
+        BinaryContent created = binaryContentService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
     @GetMapping("/find")
     public ResponseEntity<BinaryContent> find(
             @RequestParam UUID binaryContentId
@@ -22,6 +32,13 @@ public class BinaryContentController {
         BinaryContent binaryContent = binaryContentService.find(binaryContentId);
         return ResponseEntity.ok(binaryContent);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        binaryContentService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
 
