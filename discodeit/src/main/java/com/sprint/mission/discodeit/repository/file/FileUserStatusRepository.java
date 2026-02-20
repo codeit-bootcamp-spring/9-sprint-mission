1de5178a73b83687c6d7646253e5b009d9521913
전명훈 <acekid2120@gmail.com>
2026-02-20 14:50:37 +0900

    apply file lock FileUserStatusRepo

--- discodeit/src/main/java/com/sprint/mission/discodeit/repository/file/FileUserStatusRepository.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/repository/file/FileUserStatusRepository.java
@@ -6,13 +6,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
 
 @ConditionalOnProperty(name = "discodeit.repository.type", havingValue = "file")
@@ -20,10 +25,14 @@ import java.util.stream.Stream;
 public class FileUserStatusRepository implements UserStatusRepository {
     private final Path DIRECTORY;
     private final String EXTENSION = ".ser";
+    private final FileLockProvider fileLockProvider;
 
     public FileUserStatusRepository(
-            @Value("${discodeit.repository.file-directory:data}") String fileDirectory
+            @Value("${discodeit.repository.file-directory:data}") String fileDirectory,
+            FileLockProvider fileLockProvider
     ) {
+        this.fileLockProvider = fileLockProvider;
+
         this.DIRECTORY = Paths.get(System.getProperty("user.dir"), fileDirectory, UserStatus.class.getSimpleName());
         if (Files.notExists(DIRECTORY)) {
             try {
@@ -41,32 +50,42 @@ public class FileUserStatusRepository implements UserStatusRepository {
     @Override
     public UserStatus save(UserStatus userStatus) {
         Path path = resolvePath(userStatus.getId());
+
+        ReentrantLock lock = fileLockProvider.getLock(path);
+        lock.lock();
         try (
                 FileOutputStream fos = new FileOutputStream(path.toFile());
                 ObjectOutputStream oos = new ObjectOutputStream(fos)
         ) {
             oos.writeObject(userStatus);
+            return userStatus;
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
         }
-        return userStatus;
     }
 
     @Override
     public Optional<UserStatus> findById(UUID id) {
-        UserStatus userStatusNullable = null;
         Path path = resolvePath(id);
-        if (Files.exists(path)) {
-            try (
-                    FileInputStream fis = new FileInputStream(path.toFile());
-                    ObjectInputStream ois = new ObjectInputStream(fis)
-            ) {
-                userStatusNullable = (UserStatus) ois.readObject();
-            } catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+        if (Files.notExists(path)) {
+            return Optional.empty();
         }
-        return Optional.ofNullable(userStatusNullable);
+
+        ReentrantLock lock = fileLockProvider.getLock(path);
+        lock.lock();
+        try (
+                FileInputStream fis = new FileInputStream(path.toFile());
+                ObjectInputStream ois = new ObjectInputStream(fis)
+        ) {
+            UserStatus userStatus = (UserStatus) ois.readObject();
+            return Optional.of(userStatus);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
@@ -82,6 +101,8 @@ public class FileUserStatusRepository implements UserStatusRepository {
             return paths
                     .filter(path -> path.toString().endsWith(EXTENSION))
                     .map(path -> {
+                        ReentrantLock lock = fileLockProvider.getLock(path);
+                        lock.lock();
                         try (
                                 FileInputStream fis = new FileInputStream(path.toFile());
                                 ObjectInputStream ois = new ObjectInputStream(fis)
@@ -89,6 +110,8 @@ public class FileUserStatusRepository implements UserStatusRepository {
                             return (UserStatus) ois.readObject();
                         } catch (IOException | ClassNotFoundException e) {
                             throw new RuntimeException(e);
+                        } finally {
+                            lock.unlock();
                         }
                     })
                     .toList();
@@ -106,15 +129,22 @@ public class FileUserStatusRepository implements UserStatusRepository {
     @Override
     public void deleteById(UUID id) {
         Path path = resolvePath(id);
+
+        ReentrantLock lock = fileLockProvider.getLock(path);
+        lock.lock();
         try {
             Files.delete(path);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
         }
     }
 
     @Override
     public void deleteByUserId(UUID userId) {
+        // findAll() 내부에서 각 파일별 lock을 걸고 읽고 있으니,
+        // 여기서는 조회 후 해당 파일 deleteById()에서 lock을 걸면 충분
         this.findByUserId(userId)
                 .ifPresent(userStatus -> this.deleteById(userStatus.getId()));
     }
