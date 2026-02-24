30198b4fade2b4705e16604a98c2f65dd735b587
정혁조 <dy960508@naver.com>
2026-02-24 16:03:53 +0900

    자바 버전 변경 다시

--- discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DiscodeitApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(DiscodeitApplication.class, args);
   }
 }
