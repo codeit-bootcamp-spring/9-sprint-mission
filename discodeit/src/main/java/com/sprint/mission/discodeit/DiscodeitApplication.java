b11c1946f9caa55218d159907b1a8238fef428df
정혁조 <dy960508@naver.com>
2026-02-26 20:08:05 +0900

    재설정

--- discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscodeitApplication {
-
   public static void main(String[] args) {
-
     SpringApplication.run(DiscodeitApplication.class, args);
   }
 }
