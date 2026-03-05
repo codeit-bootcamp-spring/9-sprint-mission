515d28c78c5819f7d848e95535e882f9b2e61ad5
이용일 <lyi980403>
2026-03-05 19:08:32 +0900

    엔티티ì 수정

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -29,7 +29,7 @@ public class User implements Serializable {
     this.email = email;
     this.password = password;
     this.profileId = profileId;
-    this.profile = profile;
+
   }
 
   public void update(String newUsername, String newEmail, String newPassword, UUID newProfileId) {
