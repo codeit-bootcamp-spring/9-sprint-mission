de80011b4703a22edd21480776b2d5812ae7da49
윤성준 <jseong013@gmail.com>
2026-03-10 18:23:38 +0900

    fix: add missing dto data and storage packages

--- discodeit/src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
+++ discodeit/src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
@@ -3,11 +3,18 @@ package com.sprint.mission.discodeit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.sql.init.mode=never",
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1"
+    }
+)
 class DiscodeitApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+    // Simple context loading test
+  }
 
 }
