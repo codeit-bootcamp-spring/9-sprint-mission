99719c749a327292b38110cb39b6fadcfade894c
전명훈 <acekid2120@gmail.com>
2026-03-10 15:37:06 +0900

    MapStruct 적용

--- discodeit/src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -1,30 +1,15 @@
 package com.sprint.mission.discodeit.mapper;
 
-import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.entity.User;
-import java.util.Optional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Component
-@RequiredArgsConstructor
-public class UserMapper {
+// Profile 매핑을 위해 BinaryContentMapper를 사용하도록 지정
+@Mapper(componentModel = "spring", uses = {BinaryContentMapper.class})
+public interface UserMapper {
 
-    private final BinaryContentMapper binaryContentMapper;
-
-    public UserDto toDto(User user) {
-        Boolean online = user.getStatus().isOnline();
-        BinaryContentDto profile = Optional.ofNullable(user.getProfile())
-                .map(binaryContentMapper::toDto)
-                .orElse(null);
-
-        return new UserDto(
-                user.getId(),
-                user.getUsername(),
-                user.getEmail(),
-                profile,
-                online
-        );
-    }
+    // User 엔티티의 status 객체 안에 있는 online 값을 UserDto의 online으로 매핑
+    @Mapping(target = "online", source = "status.online")
+    UserDto toDto(User user);
 }
