a1e0ea9a99dc01ae2aa3ad6ac2388131e620d263
한성재 <anis15740@gmail.com>
2026-03-25 18:04:25 +0900

    GlobalExceptionHandler 수정

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/exception/ErrorResponse.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/exception/ErrorResponse.java
@@ -1,15 +1,25 @@
 package com.sprint.mission.discodeit.dto.exception;
 
+import com.sprint.mission.discodeit.exception.base.DiscodeitException;
 import java.time.Instant;
 import java.util.Map;
-import java.util.Objects;
 
 public record ErrorResponse(
     Instant timestamp,
     String code,
     String message,
-    Map<String, Objects> details,
+    Map<String, Object> details,
     String exceptionType,
     int status
 ) {
+  public static ErrorResponse of(DiscodeitException ex, int status) {
+    return new ErrorResponse(
+        ex.getTimestamp(),
+        ex.getErrorCode().name(),
+        ex.getMessage(),
+        ex.getDetails(),
+        ex.getClass().getSimpleName(),
+        status
+    );
+  }
 }
--- discodeit/src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,43 +1,93 @@
 package com.sprint.mission.discodeit.exception;
 
-import com.sprint.mission.discodeit.dto.exception.ApiError;
+import com.sprint.mission.discodeit.dto.exception.ErrorResponse;
+import com.sprint.mission.discodeit.exception.base.DiscodeitException;
+import com.sprint.mission.discodeit.exception.channel.ChannelNotFoundException;
+import com.sprint.mission.discodeit.exception.channel.PrivateChannelUpdateException;
+import com.sprint.mission.discodeit.exception.message.MessageNotFoundException;
+import com.sprint.mission.discodeit.exception.user.UserAlreadyExistsException;
+import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
+import java.time.Instant;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.NoSuchElementException;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ApiError> handleIllegalArgument(IllegalArgumentException e) {
-        ApiError error = new ApiError("SERVER ERROR", "IllegalArgumentException - " + e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ErrorResponse response = new ErrorResponse(
+            Instant.now(),
+            "INTERNAL_SERVER_ERROR",
+            "서버 내부 오류가 발생했습니다 - " + e.getMessage(),
+            null,
+            e.getClass().getSimpleName(),
+            status.value()
+        );
+        return ResponseEntity
+            .status(status)
+            .body(response);
     }
 
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ApiError> handleAuthenticationException(AuthenticationException e) {
-        ApiError error = new ApiError("SERVER ERROR", "AuthenticationException - " + e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    // custom exception
+    @ExceptionHandler(DiscodeitException.class)
+    public ResponseEntity<ErrorResponse> handleDiscodeitException(DiscodeitException e){
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ErrorResponse response = ErrorResponse.of(e, status.value());
+        return ResponseEntity
+            .status(status)
+            .body(response);
+    }
+    // User
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse response = ErrorResponse.of(e, status.value());
+        return ResponseEntity
+            .status(status)
+            .body(response);
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ApiError> handleRuntime(RuntimeException e) {
-        ApiError error = new ApiError("SERVER_ERROR", "RuntimeException - " + e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(UserAlreadyExistsException e){
+        HttpStatus status = HttpStatus.CONFLICT;
+        ErrorResponse response = ErrorResponse.of(e, status.value());
+        return ResponseEntity
+            .status(status)
+            .body(response);
     }
 
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ApiError> handleNoSuchElementException(NoSuchElementException e){
-        ApiError error = new ApiError("SERVER_ERROR", "NoSuchElementException - " + e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    // Channel
+    @ExceptionHandler(ChannelNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleChannelNotFoundException(ChannelNotFoundException e){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse response = ErrorResponse.of(e, status.value());
+        return ResponseEntity
+            .status(status)
+            .body(response);
     }
 
-    @ExceptionHandler(UploadFileException.class)
-    public ResponseEntity<ApiError> handleUploadFileException(UploadFileException e){
-        ApiError error = new ApiError("SERVER_ERROR", "UploadFileException - " + e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    @ExceptionHandler(PrivateChannelUpdateException.class)
+    public ResponseEntity<ErrorResponse> handlePrivateChannelUpdateException(PrivateChannelUpdateException e){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse response = ErrorResponse.of(e, status.value());
+        return ResponseEntity
+            .status(status)
+            .body(response);
+    }
+    
+    // Message
+    @ExceptionHandler(MessageNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleChannelNotFoundException(MessageNotFoundException e){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse response = ErrorResponse.of(e, status.value());
+        return ResponseEntity
+            .status(status)
+            .body(response);
     }
 }
