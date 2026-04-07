50e1c1d59945baf045e34894b6d81cec4eaa6434
한성재 <anis15740@gmail.com>
2026-04-07 11:12:05 +0900

    BinaryContentStorage 고도화

--- discodeit/src/test/java/com/sprint/mission/discodeit/integration/MessageIntegrationTest.java
+++ discodeit/src/test/java/com/sprint/mission/discodeit/integration/MessageIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.sprint.mission.discodeit.integraton;
+package com.sprint.mission.discodeit.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
--- discodeit/src/test/java/com/sprint/mission/discodeit/integration/UserIntegrationTest.java
+++ discodeit/src/test/java/com/sprint/mission/discodeit/integration/UserIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.sprint.mission.discodeit.integraton;
+package com.sprint.mission.discodeit.integration;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
