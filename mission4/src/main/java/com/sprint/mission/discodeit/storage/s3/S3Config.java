6b43429d9b6752723e34ca06765628f66697c794
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-04-08 21:42:00 +0900

    chore: gitignore수정

---
+++ mission4/src/main/java/com/sprint/mission/discodeit/storage/s3/S3Config.java
@@ -0,0 +1,30 @@
+package com.sprint.mission.discodeit.storage.s3;
+
+
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+  @Value("${discodeit.storage.s3.access-key}")
+  private String accessKey;
+  @Value("${discodeit.storage.s3.secret-key}")
+  private String secretKey;
+  @Value("${discodeit.storage.s3.region}")
+  private String region;
+  @Value("${discodeit.storage.s3.bucket}")
+  private String bucket;
+
+
+  @Bean
+  @ConditionalOnProperty(name = "discodeit.storage.type", havingValue = "s3")
+  public BinaryContentStorage binaryContentStorage() {
+    return new S3BinaryContentStorage(accessKey, secretKey, region, bucket);
+  }
+
+
+}
