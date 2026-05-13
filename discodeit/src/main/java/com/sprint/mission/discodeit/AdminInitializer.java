f377a02c3501d9f1ae838ff97b0e9e33176a8f86
Alpha <mathematic026@gmail.com>
2026-05-13 22:26:44 +0900

    Feat: admin 계정 추가

---
+++ discodeit/src/main/java/com/sprint/mission/discodeit/AdminInitializer.java
@@ -0,0 +1,31 @@
+package com.sprint.mission.discodeit;
+
+import com.sprint.mission.discodeit.dto.request.UserStatusCreateRequest;
+import com.sprint.mission.discodeit.entity.Role;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer implements CommandLineRunner {
+
+  private final UserStatusService userStatusService;
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  public void run(String... args) {
+    if (!userRepository.existsByUsername("admin")) {
+      String password = passwordEncoder.encode("password1234");
+      User admin = new User("admin", "admin@service.com", password, null);
+      admin.upateRole(Role.ADMIN);
+      userRepository.save(admin);
+      userStatusService.create(new UserStatusCreateRequest(admin.getId(), admin.getCreatedAt()));
+    }
+  }
+}
