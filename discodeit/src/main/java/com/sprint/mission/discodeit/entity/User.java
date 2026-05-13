07df6e6244fd30880d0a0e92960c958ac7b7dd8d
Alpha <mathematic026@gmail.com>
2026-05-13 22:27:00 +0900

    Refactor: 기본 사용자 권한 'USER'

--- discodeit/src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -41,6 +41,7 @@ public class User extends BaseUpdatableEntity {
     this.email = email;
     this.password = password;
     this.profile = profile;
+    this.role = Role.USER;
   }
 
   public void update(String newUsername, String newEmail, String newPassword,
