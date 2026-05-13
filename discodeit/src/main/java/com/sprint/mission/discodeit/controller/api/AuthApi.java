b840b21a251c3eb987645204fe3f330bf2e11c54
Alpha <mathematic026@gmail.com>
2026-05-13 22:04:18 +0900

    Feat: 로그인 로직 삭제 및 csrf, token, 권한 변경 기능 추가

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
@@ -1,36 +1,26 @@
 package com.sprint.mission.discodeit.controller.api;
 
+import com.sprint.mission.discodeit.dto.data.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.dto.data.UserDto;
-import com.sprint.mission.discodeit.dto.request.LoginRequest;
+import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Auth", description = "인증 API")
 public interface AuthApi {
 
-  @Operation(summary = "로그인")
-  @ApiResponses(value = {
-      @ApiResponse(
-          responseCode = "200", description = "로그인 성공",
-          content = @Content(schema = @Schema(implementation = UserDto.class))
-      ),
-      @ApiResponse(
-          responseCode = "404", description = "사용자를 찾을 수 없음",
-          content = @Content(examples = @ExampleObject(value = "User with username {username} not found"))
-      ),
-      @ApiResponse(
-          responseCode = "400", description = "비밀번호가 일치하지 않음",
-          content = @Content(examples = @ExampleObject(value = "Wrong password"))
-      )
-  })
-  ResponseEntity<UserDto> login(
-      @Parameter(description = "로그인 정보") LoginRequest loginRequest
-  );
-} 
+  @GetMapping("csrf-token")
+  ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken);
+
+  @Operation(summary = "현재 사용자 정보 조회", description = "세션 ID를 기반으로 현재 로그인된 사용자의 기본 정보를 반환합니다.")
+  ResponseEntity<UserDto> me(@AuthenticationPrincipal DiscodeitUserDetails userDetails);
+
+  @PutMapping("role")
+  ResponseEntity<UserDto> role(@RequestBody RoleUpdateRequest request);
+}
