ecae50cc53fc9e0a02589191bee1dbf4e57f6bf8
nano-mm <nano123@gachon.ac.kr>
2026-05-14 10:09:23 +0900

    feat: 현재 사용자 조회 API(/api/auth/me) 추가

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +22,13 @@ public class AuthController {
     String tokenValue = csrfToken.getToken();
     logger.debug("CSRF 토큰 요청: {}", tokenValue);
     return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<UserDto> me(
+      @AuthenticationPrincipal DiscodeitUserDetails userDetails
+  ) {
+
+    return ResponseEntity.ok(userDetails.getUserDto());
   }
 }
