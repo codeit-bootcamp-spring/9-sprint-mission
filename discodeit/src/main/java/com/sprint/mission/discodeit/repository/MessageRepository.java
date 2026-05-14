daf2d8989e8edf26ac2c5abc31d23711e857d51a
Alpha <mathematic026@gmail.com>
2026-05-14 15:05:58 +0900

    Refactor: UserStatus 삭제

--- discodeit/src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -13,19 +13,18 @@ import org.springframework.data.repository.query.Param;
 public interface MessageRepository extends JpaRepository<Message, UUID> {
 
   @Query("SELECT m FROM Message m "
-      + "LEFT JOIN FETCH m.author a "
-      + "JOIN FETCH a.status "
-      + "LEFT JOIN FETCH a.profile "
-      + "WHERE m.channel.id=:channelId AND m.createdAt < :createdAt")
+         + "LEFT JOIN FETCH m.author a "
+         + "LEFT JOIN FETCH a.profile "
+         + "WHERE m.channel.id=:channelId AND m.createdAt < :createdAt")
   Slice<Message> findAllByChannelIdWithAuthor(@Param("channelId") UUID channelId,
       @Param("createdAt") Instant createdAt,
       Pageable pageable);
 
 
   @Query("SELECT m.createdAt "
-      + "FROM Message m "
-      + "WHERE m.channel.id = :channelId "
-      + "ORDER BY m.createdAt DESC LIMIT 1")
+         + "FROM Message m "
+         + "WHERE m.channel.id = :channelId "
+         + "ORDER BY m.createdAt DESC LIMIT 1")
   Optional<Instant> findLastMessageAtByChannelId(@Param("channelId") UUID channelId);
 
   void deleteAllByChannelId(UUID channelId);
--- discodeit/src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -14,10 +14,9 @@ public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
   List<ReadStatus> findAllByUserId(UUID userId);
 
   @Query("SELECT r FROM ReadStatus r "
-      + "JOIN FETCH r.user u "
-      + "JOIN FETCH u.status "
-      + "LEFT JOIN FETCH u.profile "
-      + "WHERE r.channel.id = :channelId")
+         + "JOIN FETCH r.user u "
+         + "LEFT JOIN FETCH u.profile "
+         + "WHERE r.channel.id = :channelId")
   List<ReadStatus> findAllByChannelIdWithUser(@Param("channelId") UUID channelId);
 
   Optional<ReadStatus> findByUserIdAndChannelId(UUID userId, UUID channelId);
