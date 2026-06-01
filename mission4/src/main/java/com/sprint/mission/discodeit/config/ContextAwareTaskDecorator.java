914095c1e5e31c4990cc74b65d67d44075827360
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-06-01 13:59:04 +0900

    feature:AsyncConfig 클래스 구현 및 ContextAwareTaskDecorator구현

---
+++ mission4/src/main/java/com/sprint/mission/discodeit/config/ContextAwareTaskDecorator.java
@@ -0,0 +1,30 @@
+package com.sprint.mission.discodeit.config;
+
+import java.util.Map;
+import org.slf4j.MDC;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class ContextAwareTaskDecorator implements TaskDecorator {
+
+  @Override
+  public Runnable decorate(Runnable runnable) {
+    Map<String, String> contextMap = MDC.getCopyOfContextMap();
+    SecurityContext securityContext = SecurityContextHolder.getContext();
+
+    return () -> {
+      try {
+        if (contextMap != null) {
+          MDC.setContextMap(contextMap);
+        }
+        SecurityContextHolder.setContext(securityContext);
+        runnable.run();
+      } finally {
+        MDC.clear();
+        SecurityContextHolder.clearContext();
+      }
+    };
+  }
+
+}
