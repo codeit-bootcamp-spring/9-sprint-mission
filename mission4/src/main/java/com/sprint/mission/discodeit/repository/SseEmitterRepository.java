86b768952ab663432a32e9d9a7cb37d9d68fcb50
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-06-16 13:15:32 +0900

    feature:SseEmtterRepository 구현

---
+++ mission4/src/main/java/com/sprint/mission/discodeit/repository/SseEmitterRepository.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.repository;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Repository
+public class SseEmitterRepository {
+
+  private final ConcurrentMap<UUID, List<SseEmitter>> data = new ConcurrentHashMap<>();
+
+  public void save(UUID receiverId, SseEmitter sseEmitter) {
+    data.computeIfAbsent(receiverId, k -> new CopyOnWriteArrayList<>()).add(sseEmitter);
+  }
+
+  public List<SseEmitter> findAll(UUID receiverId) {
+    return data.getOrDefault(receiverId, List.of());
+  }
+
+  public void remove(UUID receiverId, SseEmitter sseEmitter) {
+    List<SseEmitter> emitters = data.get(receiverId);
+    if (emitters != null) {
+      emitters.remove(sseEmitter);
+      if (emitters.isEmpty()) {
+        data.remove(receiverId);
+      }
+    }
+
+  }
+
+}
