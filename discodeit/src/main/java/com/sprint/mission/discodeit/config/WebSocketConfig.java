333d6882d00517c3168bc2fcfe8d3660e126a5ea
apple <kjw010900@gmail.com>
2026-06-17 19:05:02 +0900

    feat: add JwtAuthenticationChannelInterceptor for WebSocket security and authorization

--- discodeit/src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -1,14 +1,24 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.secure.JwtAuthenticationChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import com.sprint.mission.discodeit.entity.UserRole;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  private final JwtAuthenticationChannelInterceptor jwtAuthenticationChannelInterceptor;
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -20,5 +30,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws").withSockJS();
   }
+
+  private AuthorizationChannelInterceptor authorizationChannelInterceptor() {
+    return new AuthorizationChannelInterceptor(
+        MessageMatcherDelegatingAuthorizationManager.builder()
+            .anyMessage().hasRole(UserRole.USER.name())
+            .build()
+    );
+  }
+
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+    registration.interceptors(
+        jwtAuthenticationChannelInterceptor,
+        new SecurityContextChannelInterceptor(),
+        authorizationChannelInterceptor()
+    );
+  }
+
 
 }
